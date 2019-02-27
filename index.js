f34cab6cbd7fb0183865b09351413e55b03d2b1f
Mohammed Noori <kmanoorib@gmail.com>
2019-02-27 03:40:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-function Scooter () {
-  
+function Scooter (year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
 }
-
-Scooter constructor
-should create a new scooter with three properties ‣
-ReferenceError: Scooter is not defined
-    at Context.<anonymous> (test/indexTest.js:6:20)let samson = new Scooter(2015, "red", "swoosh");
-expect(samson).to.be.an.instanceof(Scooter);
-expect(samson.year).to.equal(2015);
-expect(samson.color).to.equal("red");
-expect(samson.model).to.equal("swoosh");
